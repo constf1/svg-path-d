4e234aee8eb12d31256e9e483a76b8a83d124115
mico <mi-const@yandex.ru>
2021-01-25 15:57:42 +0300

    - parser.DrawToken

--- src/path-node.ts
+++ src/path-node.ts
@@ -1,6 +1,6 @@
-import { DrawTo, MoveTo } from './command';
+import { createDrawItem, DrawCommand, DrawTo, MoveTo } from './command';
 import { isClosePath, isHLineTo, isMoveTo, isVLineTo } from './command-assertion';
-import { DrawToken } from './parser';
+import { applyTranslate } from './in-place-transform';
 
 export type PathNode = DrawTo & { prev?: PathNode };
 
@@ -38,49 +38,18 @@ export function getY(item?: Readonly<PathNode>): number {
   return 0;
 }
 
-export function createPathNode({ name, args, relative }: Readonly<DrawToken>, prev?: PathNode): PathNode {
-  const X0 = relative ? getX(prev) : 0;
-  const Y0 = relative ? getY(prev) : 0;
-
-  switch (name) {
-    case 'Z':
-      return { name, prev };
-    case 'H':
-      return { name, x: +args[0] + X0, prev };
-    case 'V':
-      return { name, y: +args[0] + Y0, prev };
-    case 'M':
-    case 'L':
-    case 'T':
-      return { name, x: +args[0] + X0, y: +args[1] + Y0, prev };
-    case 'Q':
-      return { name, x1: +args[0] + X0, y1: +args[1] + Y0, x: +args[2] + X0, y: +args[3] + Y0, prev };
-    case 'S':
-      return { name, x2: +args[0] + X0, y2: +args[1] + Y0, x: +args[2] + X0, y: +args[3] + Y0, prev };
-    case 'C':
-      return {
-        name,
-        x1: +args[0] + X0,
-        y1: +args[1] + Y0,
-        x2: +args[2] + X0,
-        y2: +args[3] + Y0,
-        x: +args[4] + X0,
-        y: +args[5] + Y0,
-        prev,
-      };
-    case 'A':
-      return {
-        name,
-        rx: +args[0],
-        ry: +args[1],
-        angle: +args[2],
-        largeArcFlag: +args[3] === 1,
-        sweepFlag: +args[4] === 1,
-        x: +args[5] + X0,
-        y: +args[6] + Y0,
-        prev,
-      };
+export function createPathNode(
+  name: DrawCommand,
+  args: ReadonlyArray<string | number>,
+  relative?: boolean,
+  prev?: PathNode
+): PathNode {
+  const node: PathNode = createDrawItem(name, args);
+  node.prev = prev;
+  if (relative && prev) {
+    applyTranslate(node, getX(prev), getY(prev));
   }
+  return node;
 }
 
 /**
