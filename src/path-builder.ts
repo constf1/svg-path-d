53869dec5899f720cf1be3618c4f48831f112e67
mico <mi-const@yandex.ru>
2021-01-25 16:00:59 +0300

    small refactoring

--- src/path-builder.ts
+++ src/path-builder.ts
@@ -6,102 +6,118 @@ export class PathBuilder {
     return this.path[this.path.length - 1];
   }
 
+  get lastX(): number {
+    return getX(this.last);
+  }
+
+  get lastY(): number {
+    return getY(this.last);
+  }
+
   constructor(readonly path: PathNode[] = []) {}
 
-  M(x: number, y: number): this {
-    this.path.push({ name: 'M', x, y, prev: this.last });
+  push(next: PathNode): this {
+    next.prev = this.last;
+    this.path.push(next);
     return this;
   }
 
+  /**
+   * Adds `MoveTo` command to the path.
+   * @param x absolute x coordinate to move to.
+   * @param y absolute y coordinate to move to.
+   */
+  M(x: number, y: number): this {
+    return this.push({ name: 'M', x, y });
+  }
+
+  /**
+   * Adds `LineTo` command to the path.
+   * @param x absolute x coordinate to draw line to.
+   * @param y absolute y coordinate to draw line to.
+   */
   L(x: number, y: number): this {
-    this.path.push({ name: 'L', x, y, prev: this.last });
-    return this;
+    return this.push({ name: 'L', x, y });
   }
 
   H(x: number): this {
-    this.path.push({ name: 'H', x, prev: this.last });
-    return this;
+    return this.push({ name: 'H', x });
   }
 
   V(y: number): this {
-    this.path.push({ name: 'V', y, prev: this.last });
-    return this;
+    return this.push({ name: 'V', y });
   }
 
   C(x1: number, y1: number, x2: number, y2: number, x: number, y: number): this {
-    this.path.push({ name: 'C', x1, y1, x2, y2, x, y, prev: this.last });
-    return this;
+    return this.push({ name: 'C', x1, y1, x2, y2, x, y });
   }
 
   S(x2: number, y2: number, x: number, y: number): this {
-    this.path.push({ name: 'S', x2, y2, x, y, prev: this.last });
-    return this;
+    return this.push({ name: 'S', x2, y2, x, y });
   }
 
   Q(x1: number, y1: number, x: number, y: number): this {
-    this.path.push({ name: 'Q', x1, y1, x, y, prev: this.last });
-    return this;
+    return this.push({ name: 'Q', x1, y1, x, y });
   }
 
   T(x: number, y: number): this {
-    this.path.push({ name: 'T', x, y, prev: this.last });
-    return this;
+    return this.push({ name: 'T', x, y });
   }
 
-  A(rx: number, ry: number, angle: number, largeArcFlag: number, sweepFlag: number, x: number, y: number): this {
-    this.path.push({
-      name: 'A',
-      rx,
-      ry,
-      angle,
-      largeArcFlag: !!largeArcFlag,
-      sweepFlag: !!sweepFlag,
-      x,
-      y,
-      prev: this.last,
-    });
-    return this;
+  A(rx: number, ry: number, angle: number, largeArc: number, sweep: number, x: number, y: number): this {
+    const largeArcFlag = !!largeArc;
+    const sweepFlag = !!sweep;
+    return this.push({ name: 'A', rx, ry, angle, largeArcFlag, sweepFlag, x, y });
   }
 
   Z(): this {
-    this.path.push({ name: 'Z', prev: this.last });
-    return this;
+    return this.push({ name: 'Z' });
   }
 
-  m(x: number, y: number): this {
-    return this.M(x, y)._relative();
+  /**
+   * Adds `MoveTo` command to the path.
+   * @param dx relative x coordinate to move to.
+   * @param dy relative y coordinate to move to.
+   */
+  m(dx: number, dy: number): this {
+    return this.M(dx, dy)._relative();
   }
 
-  l(x: number, y: number): this {
-    return this.L(x, y)._relative();
+  /**
+   * Adds `LineTo` command to the path.
+   * @param dx relative x coordinate to draw line to.
+   * @param dy relative y coordinate to draw line to.
+   */
+  l(dx: number, dy: number): this {
+    return this.L(dx, dy)._relative();
   }
 
-  h(x: number): this {
-    return this.H(x)._relative();
+  h(dx: number): this {
+    return this.H(dx)._relative();
   }
 
-  v(y: number): this {
-    return this.V(y)._relative();
+  v(dy: number): this {
+    return this.V(dy)._relative();
   }
 
-  c(x1: number, y1: number, x2: number, y2: number, x: number, y: number): this {
-    return this.C(x1, y1, x2, y2, x, y)._relative();
+  c(dx1: number, dy1: number, dx2: number, dy2: number, dx: number, dy: number): this {
+    return this.C(dx1, dy1, dx2, dy2, dx, dy)._relative();
   }
 
-  s(x2: number, y2: number, x: number, y: number): this {
-    return this.S(x2, y2, x, y)._relative();
+  s(dx2: number, dy2: number, dx: number, dy: number): this {
+    return this.S(dx2, dy2, dx, dy)._relative();
   }
 
-  q(x1: number, y1: number, x: number, y: number): this {
-    return this.Q(x1, y1, x, y)._relative();
+  q(dx1: number, dy1: number, dx: number, dy: number): this {
+    return this.Q(dx1, dy1, dx, dy)._relative();
   }
 
-  t(x: number, y: number): this {
-    return this.T(x, y)._relative();
+  t(dx: number, dy: number): this {
+    return this.T(dx, dy)._relative();
   }
 
-  a(rx: number, ry: number, angle: number, largeArcFlag: number, sweepFlag: number, x: number, y: number): this {
-    return this.A(rx, ry, angle, largeArcFlag, sweepFlag, x, y)._relative();
+  a(rx: number, ry: number, angle: number, largeArc: number, sweep: number, dx: number, dy: number): this {
+    return this.A(rx, ry, angle, largeArc, sweep, dx, dy)._relative();
   }
 
   z(): this {
